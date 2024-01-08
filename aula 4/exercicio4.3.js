46cd93a69eaa113e750245949f9f377c9aaca23b
Lucas Santiago <lucahh@gmail.com>
2024-01-08 14:27:23 -0300

    exercicio 4
    termino do exercicio

--- aula 4/exercicio4.3.js
+++ aula 4/exercicio4.3.js
@@ -2,6 +2,10 @@ function aplicarDesconto(valor, desconto){
     return (valor - (valor *(desconto / 100)));
 }
 
+function aplicarJuros(valor, juros){
+    return (valor + (valor * (juros / 100)))
+}
+
 const precoEtiqueta = 100;
 const formaPagamento = 4;
 
@@ -9,7 +13,8 @@ if (formaPagamento === 1){
     console.log(aplicarDesconto(precoEtiqueta, 10));
 }else if (formaPagamento === 2){
     console.log(aplicarDesconto(precoEtiqueta, 15));
-}else if (formaPagamento === 1){
+}else if (formaPagamento === 3){
     console.log(precoEtiqueta);
+}else {
+    console.log(aplicarJuros(precoEtiqueta, 10));
 }
-
