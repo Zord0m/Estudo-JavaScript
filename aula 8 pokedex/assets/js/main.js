e3edf0ca2ea5e23cc9510c0dd555e7718f54389c
Lucas Santiago <lucahh@gmail.com>
2024-02-01 22:41:20 -0300

    Continuação
    continuação das requisições de API

--- aula 8 pokedex/assets/js/main.js
+++ aula 8 pokedex/assets/js/main.js
@@ -3,6 +3,13 @@ const offset =0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(response){
+fetch(url)
+    .then(function(response){
     console.log(response)
-})
+    })
+    .catch(function (error){
+        console.error(error)
+    })
+    .finally(function(){
+        console.log('Requisição concluída.')
+    })
