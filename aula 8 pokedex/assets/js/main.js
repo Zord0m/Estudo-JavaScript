3defd74709e9f16b5d465444ab61ae20d45b8f63
Lucas Santiago <lucahh@gmail.com>
2024-02-02 15:34:43 -0300

    Continuação
    continuação

--- aula 8 pokedex/assets/js/main.js
+++ aula 8 pokedex/assets/js/main.js
@@ -3,13 +3,40 @@ const offset =0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon) {
+    return`
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>    
+        </li>
+    `
+}
+
+const pokemonList = document.getElementsByClassName('pokemonList')
+pokemonList.appendChild('<li></li>')
+
 fetch(url)
-    .then(function(response){
-    console.log(response)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+
+        for (let i = 0; i < pokemonList.length; i++) {
+            const element = pokemonList[i];
+            console.log(convertPokemonToLi(pokemon))
+
+            
+            
+        }
     })
-    .catch(function (error){
-        console.error(error)
-    })
-    .finally(function(){
-        console.log('Requisição concluída.')
-    })
+    .catch((error) => console.error(error))
+    
