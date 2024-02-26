d24dd3d6feed1b4d683545d5bf413c513ee57a34
Lucas Santiago <lucahh@gmail.com>
2024-02-26 16:26:58 -0300

    atualização
    blog

---
+++ aula 12 - angular-blog/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do Pantera Negra será lançado em breve",
+    "description": "Novo filme do Pantera Negra será lançado em breve e contará com um elenco de grande influencia!",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "Novo filme dos X-men está em pós produção, afirma diretor e contará com os uniformes originais dos quadrinhos da equipe de mutantes.",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  }
+]
